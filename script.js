5ad4a0dfceab3733a863b25d18878829a500ceff
Ahmad Zayab <106998360+ahmad-zayab@users.noreply.github.com>
2025-12-18 12:25:42 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function User() {
+  const nameInput = document.getElementById("userName");
+  const city = nameInput.value;
+
+  const apiKey = "1972e28b70cf290bbb0a8b2e3626655f";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(res => {
+      if (!res.ok) throw new Error("City not found");
+      return res.json();
+    })
+    .then(data => {
+      document.getElementById("output").textContent =
+        `City: ${data.name}
+Temperature: ${data.main.temp} °C
+Weather: ${data.weather[0].description}`;
+    })
+    .catch(err => {
+      document.getElementById("output").textContent = err.message;
+    });
+}
